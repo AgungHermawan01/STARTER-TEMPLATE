419587c1aa5429d2591b7fee31cc640f5aaebfb8
AgungHermawan01 <agung98hermawan@gmail.com>
2022-11-04 19:51:55 -0700

    Login

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,8 @@ Auth::routes();
 Route::get('/home', function() {
     return view('home');
 })->name('home')->middleware('auth');
+
+Route::get('admin/home', [App\Http\Controllers\AdminController::class, 'index'])
+            ->name('admin.home')
+            ->middleware('is_admin'); 
+   
