6da48b6a02dc6d2c80c275c0d8acd0a358977710
AgungHermawan01 <agung98hermawan@gmail.com>
2022-12-16 10:57:16 +0700

    Tampilan Modal Tambah Data

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -61,4 +61,45 @@
         </div>
     </div>
 </div>
+<div class="modal fade" id="tambahBukuModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Buku</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form method="post" action="{{ route('admin.book.submit') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <label for="judul">Judul Buku</label>
+                        <input type="text" class="form-control" name="judul" id="judul" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="penulis">Penulis</label>
+                        <input type="text" class="form-control" name="penulis" id="penulis" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="tahun">Tahun</label>
+                        <input type="year" class="form-control" name="tahun" id="tahun" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="penerbit">Penerbit</label>
+                        <input type="text" class="form-control" name="penerbit" id="penerbit" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="cover">Cover</label>
+                        <input type="file" class="form-control" name="cover" id="cover" />
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dissmiss="modal">Tutup</button>
+                <button type="submit" class="btn btn-primary">Kirim</button>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
